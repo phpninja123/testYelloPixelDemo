1b59d77b52e3c486e28ee55c393f2c6c9de666e4
mayuri90 <cmc.mayuri@gmail.com>
2016-03-04 00:35:36 +0530

    dbconnection

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -2,7 +2,7 @@
 //getting database instance & connection
 	function getDBConnection(){
 		//if (!defined('constant')) define('constant', 'value');
-		if(!defined("USERNAME")) define("USERNAME", 'root');
+		if(!defined("USERNAME")) define("USERNAMM", 'root');
 		if(!defined("PASSWORD")) define("PASSWORD", 'root');
 		if(!defined("SERVER")) define("SERVER", 'ec2-52-26-13-216.us-west-2.compute.amazonaws.com');
 		if(!defined("DB")) define("DB", 'yellow_pixel');
