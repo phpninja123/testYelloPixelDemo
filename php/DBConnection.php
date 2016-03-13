0774514891b6c995fb7caea046b7f072545b6edc
Ubuntu <ubuntu@ip-172-31-21-125.us-west-2.compute.internal>
2016-03-13 16:59:50 +0000

    test

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -2,8 +2,8 @@
 //getting database instance & connection
 	function getDBConnection(){
 		//if (!defined('constant')) define('constant', 'value');
-		if(!defined("USERNAME")) define("USERNAMM", 'root');
-		if(!defined("PASSWORD")) define("PASSWORD", '');
+		if(!defined("USERNAME")) define("USERNAME", 'root');
+		if(!defined("PASSWORD")) define("PASSWORD", 'root');
 		if(!defined("SERVER")) define("SERVER", 'localhost');
 		if(!defined("DB")) define("DB", 'yellow_pixel');
 		$conn=mysqli_connect(SERVER,USERNAME,PASSWORD,DB) or die('Error in database connection');
